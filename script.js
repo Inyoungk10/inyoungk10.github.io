47b092cab9624275282d58938d04e94aec93961a
inyoungk10 <inyoungk10@gmail.com>
2022-12-10 22:44:38 -0800

    check

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function play(id) {
     var audio = new Audio();
     audio.src = "./audio/" + id + ".mp3";
     audio.play();
+    console.log("PLAYING AUDIO: " + id);
 }
 
 for (let i = 1; i < 41; i++) {
